43f30ece9c56affd6892acf30109790a3102254a
osulca <omarsulca@gmail.com>
2020-07-21 20:26:33 -0500

    ejercicio completo;

--- app/src/main/java/com/example/jsonws/MainActivity.java
+++ app/src/main/java/com/example/jsonws/MainActivity.java
@@ -11,18 +11,22 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Array;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,25 +36,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void traerDatos(View view) {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String URI = "https://jsonplaceholder.typicode.com/posts";
+        String URI1 = "https://jsonplaceholder.typicode.com/posts";
+        String URI2 = "https://gateway.marvel.com/v1/public/characters?name=thor&apikey=5c465051a788252db815c7bd342d97a1&ts=1&hash=3e46134f8ed0002366b015568a3f320c";
 
-        JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                URI,
-                null,
+        /*
+        JsonArrayRequest requestArray = new JsonArrayRequest(
+                Request.Method.GET, URI1, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        tvDatos = findViewById(R.id.tvData);
+                        tvDatos.setText("");
+                        for(int i=0; i<response.length(); i++) {
+                            try {
+                                JSONObject post = (JSONObject) response.get(i);
+                                tvDatos.append(
+                                        "userId:"+post.getString("userId")+"\n"+
+                                        "id:"+post.getString("id")+"\n"+
+                                        "title:"+post.getString("title")+"\n"+
+                                        "body:"+post.getString("body")+"\n\n"
+                                );
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ErrorRequest", error.getMessage());
+                    }
+                }
+        );
+        */
+
+        JsonObjectRequest requestObject = new JsonObjectRequest(
+                Request.Method.GET, URI2, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        TextView tvData = findViewById(R.id.tvData);
-                        tvData.setText("");
-                        JSONArray  data = null;
+                        tvDatos = findViewById(R.id.tvData);
                         try {
-                            data = response.getJSONArray("data");
-                            for(int i = 0; i<=data.length(); i++){
-                                JSONObject post = data.getJSONObject(i);
-                                tvData.append(post.length()+"\n");
-                            }
-
+                            JSONObject data = (JSONObject) response.get("data");
+                            JSONArray results = data.getJSONArray("results");
+                            JSONObject personaje = (JSONObject) results.get(0);
+                            tvDatos.setText(
+                                        "Nombre: "+ personaje.get("name")+"\n"+
+                                        "Descripcion: "+personaje.get("description")
+                                    );
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -62,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("WebService", error.toString());
                     }
                 }
-        );
-        requestQueue.add(request);
+                     );
+
+        requestQueue.add(requestObject);
     }
 }
